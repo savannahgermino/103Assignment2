cf9415e4ec1242567717da2da4250b4a5012fe90
savannahgermino <99304736+savannahgermino@users.noreply.github.com>
2022-03-04 13:28:50 -0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+productName=prompt("Enter the product name");
+quantity=prompt("Enter the product quantity");
+price=prompt("Enter the product's price");
+
+console.log(quantity,productName,price);
+var cost = (price * .16) + price;
+  console.log(cost);
